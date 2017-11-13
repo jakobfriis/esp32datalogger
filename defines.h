86aab12d54dbeaa62b4149e0aa77c7f84bb735eb
Jakob Friis <jfr@nuic7>
2017-11-13 16:34:27 +0100

    Added defines file for project Updated main project to use defines and removed som logging.

---
+++ defines.h
@@ -0,0 +1,28 @@
+#define input0 4 //GPIO04, ADC10
+#define input1 2 //GPIO02, ADC12 
+#define input2 15 //GIPO15, ADC13
+#define input3 34 //GPIO34, ADC6
+#define input4 35 //GPIO35, ADC7
+#define input5 32 //GPIO32, ADC4
+#define input6 33 //GPIO33, ADC5
+#define input7 25 //GPIO25, ADC18
+#define input8 26 //GPIO26, ADC19
+#define input9 27 //GPIO27, ADC17
+
+#define analogIn 34
+#define led 2
+
+const char* ssid     = "ASUS";
+const char* password = "76TMQEGEWD";
+const char* host = "jdata.dk";
+
+int val0=0;
+int val1=0;
+int val2=0;
+int val3=0;
+int val4=0;
+int val5=0;
+int val6=0;
+int val7=0;
+int val8=0;
+int val9=0;
